2b1f04d5985bc545139f64f3c8e0eb55d58d57b7
svinoshpatel <shpakmark@gmail.com>
2026-07-21 23:43:37 +0300

    Made docs accessible without auth

--- apps/api/src/main/java/com/github/svinoshpatel/api/config/SecurityConfig.java
+++ apps/api/src/main/java/com/github/svinoshpatel/api/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests(requests -> {
-            requests.requestMatchers( "/bid-handshake").permitAll();
+            requests.requestMatchers("/bid-handshake", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                    .permitAll();
             requests.anyRequest().authenticated();
         });
 
